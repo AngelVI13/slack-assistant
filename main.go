95fbd2d1f0647ed099f6c076984845420d5f1dc2
Angel VI <a.v.iliev13@gmail>
2022-03-17 16:49:51 +0200

    Testing commit names

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 	users := utils.GetUsers("users.json")
 	deviceManager := handlers.DeviceManager{devicesInfo, users, socketClient}
 
-	// TODO: check if multiple users can interact with the bot at the same time !!!
+	// TODO: check if multiple users can interact with the bot at the same time !
 	// Create a context that can be used to cancel goroutine
 	ctx, cancel := context.WithCancel(context.Background())
 	// Make this cancel called properly in a real program , graceful shutdown etc
